4abf3e7c94f4de906631bc0648b852b7afc9142a
Ilya Roslyakov <uprial@gmail.com>
2014-06-06 12:48:09 +0400

    Added world cut

---
+++ world_data.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+
+import os
+import sys
+
+def listfiles(dirname):
+    return [filename for filename in os.listdir(dirname) if os.path.isfile(os.path.join(dirname, filename))]
+
+def main():
+    dirname = os.path.realpath(sys.argv[0])
+    dirname = os.path.dirname(os.path.dirname(dirname))
+    dirname = os.path.join(dirname, "world")
+    dirname = os.path.join(dirname, "region")
+    for filename in listfiles(dirname):
+        parts = filename.split(".")
+        if parts[0] == "r" and parts[3] == "mca":
+            x = int(parts[1])
+            z = int(parts[2])
+            if x > 1 or x < -2 or z > 1 or z < -2:
+                #os.remove(os.path.join(dirname, filename))
+                print filename + ": " + str(x) + "," + str(z)
+
+if __name__ == "__main__":
+    main()
