865f47d5427480e6b199ede840bc991a9efe9f35
Ilya Roslyakov <uprial@gmail.com>
2014-06-06 14:30:57 +0400

    Fixed clean script

--- clean.sh
+++ clean.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-echo "ACCESS DENIED"
-exit 1
-
 set -e
+
+#echo "ACCESS DENIED"
+#exit 1
 
 cd $(dirname $(dirname $(realpath $0)))
 
-rm -rf world
-rm -rf world_nether
-rm -rf world_the_end
+#rm -rf world
+#rm -rf world_nether
+#rm -rf world_the_end
 rm -f crash-reports/*
 rm -f logs/*
 rm -f timings/*
-rm -f plugins/CustomNukes/block-meta.txt
-rm -f plugins/CustomNukes/repeater-task.txt
-sed -i 's/authkey: /authkey: ''/g' plugins/EnjinMinecraftPlugin/config.yml
-rm -rf plugins/WorldGuard/worlds
-rm -f plugins/WorldBorder/config.yml
+#rm -f plugins/CustomNukes/block-meta.txt
+#rm -f plugins/CustomNukes/repeater-task.txt
+#sed -i 's/authkey: /authkey: ''/g' plugins/EnjinMinecraftPlugin/config.yml
+#rm -rf plugins/WorldGuard/worlds
+#rm -f plugins/WorldBorder/config.yml
