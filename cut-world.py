ea35ed2a37a1546613999eaca5ee138a98f0f5c9
Ilya Roslyakov <uprial@gmail.com>
2015-03-21 18:31:56 +0300

    Fixed cut-the-world

--- cut-world.py
+++ cut-world.py
@@ -6,9 +6,16 @@ import sys
 def listfiles(dirname):
     return [filename for filename in os.listdir(dirname) if os.path.isfile(os.path.join(dirname, filename))]
 
+def k(value):
+    return value / 500
+
 def main(): 
     print "ACCESS DENIED"
     sys.exit(1)
+    x1 = k(-1000+1)
+    x2 = k(1000-1)
+    z1 = k(-1000+1)
+    z2 = k(2000-1)
     dirname = os.path.realpath(sys.argv[0])
     dirname = os.path.dirname(os.path.dirname(dirname))
     dirname = os.path.join(dirname, "world")
@@ -18,7 +25,7 @@ def main():
         if parts[0] == "r" and parts[3] == "mca":
             x = int(parts[1])
             z = int(parts[2])
-            if x > 1 or x < -2 or z > 1 or z < -2:
+            if x > x2 or x < x1 or z > z2 or z < z1:
                 os.remove(os.path.join(dirname, filename))
                 #print filename + ": " + str(x) + "," + str(z)
 
