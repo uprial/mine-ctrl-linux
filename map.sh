04af2966ceedb6e4312d7b384e5c349705aa7c47
Ilya Roslyakov <uprial@gmail.com>
2015-03-26 13:43:26 +0300

    Generate big map

--- map.sh
+++ map.sh
@@ -2,25 +2,29 @@
 
 set -e
 
-radius=1
-tiles=100
+radius=20
+tiles=125
 
 for x in $(seq -${radius} ${radius}); do
     for z in $(seq -${radius} ${radius}); do
         xo=$(( ${x} * ${tiles} * 16 ))
         zo=$(( ${z} * ${tiles} * 16 ))
-        if [[ ${xo} < 0 ]]; then
-            tx="l$(( -${xo} ))"
-        else
-            tx="r${xo}"
+
+        if [ ! -f ${tmpdir}/[${xo}_${zo}]world_biome.png ]; then
+            if [[ ${xo} < 0 ]]; then
+                tx="l$(( -${xo} ))"
+            else
+                tx="r${xo}"
+            fi
+            if [[ ${zo} < 0 ]]; then
+                tz="u$(( -${zo} ))"
+            else
+                tz="d${zo}"
+            fi
+            cmd="tc map world -s ${tiles} -o ${xo} ${zo} -l ${tx} ${tz}"
+            echo "mine> ${cmd}..."
+            $(dirname $0)/rcon-do.sh ${cmd}
         fi
-        if [[ ${zo} < 0 ]]; then
-            tz="u$(( -${zo} ))"
-        else
-            tz="d${zo}"
-        fi
-        $(dirname $0)/rcon-do.sh \
-            tc map world -s ${tiles} -o ${xo} ${zo} -l ${tx} ${tz}
     done
 done
 
@@ -42,8 +46,14 @@ for z in $(seq -${radius} ${radius}); do
     done
 done
 
+echo "Moving images..."
 mv $(dirname $(dirname $0))/*png ${tmpdir}/
 
 size=$(( ${radius} * 2 + 1 ))
-${cmd1} -geometry +0+0 -tile ${size}x ${tmpdir}/world_biome.png
-${cmd2} -geometry +0+0 -tile ${size}x ${tmpdir}/world_temperature.png
+echo "Creating world_biome.png..."
+${cmd1} -geometry +1+1 -background black -tile ${size}x ${tmpdir}/world_biome.png
+echo "Creating world_temperature.png..."
+${cmd2} -geometry +1+1 -background black -tile ${size}x ${tmpdir}/world_temperature.png
+
+cp ${tmpdir}/world_*png ~/Yandex.Disk/Minecraft/
+yandex-disk sync
