7af5c4960caba8fd5568386eb73c01b83164c70f
Ilya Roslyakov <uprial@gmail.com>
2015-03-27 18:30:14 +0300

    Fixed map generation

--- map.sh
+++ map.sh
@@ -5,6 +5,7 @@ set -e
 radius=20
 tiles=125
 tmpdir=$(dirname $0)/tmpimgs/${tiles}
+changed=0
 
 for x in $(seq -${radius} ${radius}); do
     for z in $(seq -${radius} ${radius}); do
@@ -12,24 +13,17 @@ for x in $(seq -${radius} ${radius}); do
         zo=$(( ${z} * ${tiles} * 16 ))
 
         if [ ! -f ${tmpdir}/[${xo}_${zo}]world_biome.png ]; then
-            if [[ ${xo} < 0 ]]; then
-                tx="l$(( -${xo} ))"
-            else
-                tx="r${xo}"
-            fi
-            if [[ ${zo} < 0 ]]; then
-                tz="u$(( -${zo} ))"
-            else
-                tz="d${zo}"
-            fi
-            cmd="tc map world -s ${tiles} -o ${xo} ${zo} -l ${tx} ${tz}"
+            cmd="tc map world -s ${tiles} -o ${xo} ${zo}"
             echo "mine> ${cmd}..."
             $(dirname $0)/rcon-do.sh ${cmd}
+            changed=1
         fi
     done
 done
 
-sleep 60
+if [[ "${changed}" != 0 ]]; then
+    sleep 60
+fi
 
 mkdir -p ${tmpdir}
 
@@ -45,14 +39,20 @@ for z in $(seq -${radius} ${radius}); do
     done
 done
 
-echo "Moving images..."
-mv $(dirname $(dirname $0))/*png ${tmpdir}/ || :
+if [[ "${changed}" != 0 ]]; then
+    echo "Moving images..."
+    mv $(dirname $(dirname $0))/*png ${tmpdir}/
+fi
 
 size=$(( ${radius} * 2 + 1 ))
+namesize=$(( ${tiles} * (${radius} * 16 + 8)))
+
 echo "Creating world_biome.png..."
-${cmd1} -geometry +1+1 -background black -tile ${size}x ${tmpdir}/world_biome.png
+${cmd1} -geometry +1+1 -background black -tile ${size}x \
+    ${tmpdir}/world_biome_${namesize}x${namesize}.png
 echo "Creating world_temperature.png..."
-${cmd2} -geometry +1+1 -background black -tile ${size}x ${tmpdir}/world_temperature.png
+${cmd2} -geometry +1+1 -background black -tile ${size}x \
+    ${tmpdir}/world_temperature_${namesize}x${namesize}.png
 
 cp ${tmpdir}/world_*png ~/Yandex.Disk/Minecraft/
 yandex-disk sync
