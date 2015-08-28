082f44460c080b174a87749af53eec0d0266a2df
Ilya Roslyakov <uprial@gmail.com>
2015-08-29 02:46:53 +0300

    Removed deprecated parameter in Java8

--- start-server.sh
+++ start-server.sh
@@ -4,14 +4,15 @@ set -e
 
 source `dirname $0`/config.sh
 
-VERSION="spigot-1.8.3.jar"
+VERSION="spigot-1.8.8.jar"
 echo "Starting bukkit minecraft server..."
 
 cd $(dirname $(dirname $(realpath $0)))
 
+# Work only in Java7:
+# -XX:MaxPermSize=128m
 java -server \
     -d64 \
-    -XX:MaxPermSize=128m \
     -Xmx${MEMORY_MAX} \
     -Xms${MEMORY_START} \
     -Djline.terminal=jline.UnsupportedTerminal \
