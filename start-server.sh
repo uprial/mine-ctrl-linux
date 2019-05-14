5812501241fc597f4e5b5369ef2def76c8c003bd
Ilya Roslyakov <uprial@gmail.com>
2019-05-14 16:11:50 +0300

    Added a check for Java version and fixed command line for Java 10

--- start-server.sh
+++ start-server.sh
@@ -13,12 +13,21 @@ echo "Starting bukkit minecraft server..."
 cd $(dirname $(dirname $(realpath $0)))
 
 OPTS=""
-if ! java -version 2>&1 | grep 1.8; then
-  OPTS="${OPTS} -XX:MaxPermSize=128m"
+
+java_version=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}' | cut -d'.' -f1)
+echo "Detected java version is ${java_version}..."
+if test ${java_version} -lt 6; then
+    echo "ERROR: Java version is too old."
+    exit 1
+fi
+if test ${java_version} -lt 8; then
+    OPTS="${OPTS} -XX:MaxPermSize=128m"
+fi
+if test ${java_version} -lt 10; then
+    OPTS="${OPTS} -d64"
 fi
 
 java -server \
-    -d64 \
     -Xmx${MEMORY_MAX} \
     -Xms${MEMORY_START} \
     -Djline.terminal=jline.UnsupportedTerminal \
