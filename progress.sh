cb0a0426280e9712a814b691d78ab572de662cba
Ilya Roslyakov <uprial@gmail.com>
2019-05-22 21:33:45 +0300

    Added multi-download file

--- progress.sh
+++ progress.sh
@@ -2,12 +2,22 @@
 
 set -e
 
+LINK="${1}"
+N="${2:-100000}"
+
 X=0
-N=10
+E=0
 
 while test ${X} -lt ${N}; do
-  echo -ne "${X} / ${N}...       \r"
-  X=$((${X}+1))
+  echo -ne "${X} / ${N}, ${E} errors...       \r"
+
+
+  if wget -O /dev/null ${LINK} 1>/dev/null 2>&1; then
+    X=$((${X}+1))
+  else
+    E=$((${E}+1))
+  fi
+
   sleep 1
 done
 echo "${X} / ${N} - DONE"
