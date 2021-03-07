2287a3a376a11a5d95f4e52f85d337d885670f1a
Ilya Roslyakov <uprial@gmail.com>
2021-03-07 13:18:49 +0300

    Added a wait message

--- start-server-continuous.sh
+++ start-server-continuous.sh
@@ -2,10 +2,12 @@
 
 set -e
 
+WAIT_TIME=10
 while true; do
     `dirname $0`/start-server.sh || :
     echo "`date`" >> /var/log/crashes
     echo ""
     echo ""
-    sleep 10
+    echo "Waiting for ${WAIT_TIME} seconds before restart..."
+    sleep ${WAIT_TIME}
 done
