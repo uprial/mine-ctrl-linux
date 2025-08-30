a7663184e285721a21bb1aa84048a3c35520b958
Ilya Roslyakov <uprial@gmail.com>
2025-08-30 22:41:41 +0300

    Diversified cut-world.py

--- cut-world.py
+++ cut-world.py
@@ -13,18 +13,19 @@ def g(value):
     return value * 500
 
 def main():
-    #square_radius = 7000
-    #x1 = k(-square_radius+1) - 1
-    #x2 = k(+square_radius-1) + 1
-    #z1 = k(-square_radius+1) - 1
-    #z2 = k(+square_radius-1) + 1
-    x1 = k(1010)
-    x2 = k(7510)
-    z1 = k(-10010)
-    z2 = k(-3010)
+    square_radius = 7000
+    x1 = k(-square_radius+1) - 1
+    x2 = k(+square_radius-1) + 1
+    z1 = k(-square_radius+1) - 1
+    z2 = k(+square_radius-1) + 1
+    #x1 = k(1010)
+    #x2 = k(7510)
+    #z1 = k(-10010)
+    #z2 = k(-3010)
     print ("#!/bin/bash")
     print ("set -e")
-    print ("#", ">", g(x1), "<", g(x2), ">", g(z1), "<", g(z2))
+    #print ("#", ">", g(x1), "<", g(x2), ">", g(z1), "<", g(z2))
+    print ("#", ">", g(x2), "<", g(x1), ">", g(z2), "<", g(z1))
     #exit()
     dirname = os.path.realpath(sys.argv[0])
     dirname = os.path.dirname(os.path.dirname(dirname))
@@ -37,8 +38,8 @@ def main():
         if parts[0] == "r" and parts[3] == "mca":
             x = int(parts[1])
             z = int(parts[2])
-            if x > x1 and x < x2 and z > z1 and z < z2:
-            #if x > x2 or x < x1 or z > z2 or z < z1:
+            #if x > x1 and x < x2 and z > z1 and z < z2:
+            if x > x2 or x < x1 or z > z2 or z < z1:
                 #os.remove(os.path.join(dirname, filename))
                 print("rm " + os.path.join(dirname, filename))
 
